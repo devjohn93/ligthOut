021e2ccf2aefec0379ba7d4ec87437edc403dcc6
Sagi Janos <jsagi@adverticum.com>
2019-05-18 16:50:20 +0200

    Updates

---
+++ precache-manifest.21eea2b18fe6a499e66d4ed16ef90b22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d9305ee2d4784c0d3dd",
+    "url": "/ligthOut/static/css/main.6c7cc976.chunk.css"
+  },
+  {
+    "revision": "0d9305ee2d4784c0d3dd",
+    "url": "/ligthOut/static/js/main.0d9305ee.chunk.js"
+  },
+  {
+    "revision": "a6f3a2216de1c9165847",
+    "url": "/ligthOut/static/js/1.a6f3a221.chunk.js"
+  },
+  {
+    "revision": "635d70de824b7b0e00c3",
+    "url": "/ligthOut/static/js/runtime~main.635d70de.js"
+  },
+  {
+    "revision": "d2eb448e99668210e9319a723f5c97c2",
+    "url": "/ligthOut/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/ligthOut/precache-manifest.21eea2b18fe6a499e66d4ed16ef90b22.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/ligthOut/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
